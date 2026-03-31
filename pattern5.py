72a69e4720f534672ff190141d74994d397f612b
rakshaknidhi <nidhivrakshak@gmail.com>
2026-03-31 19:55:01 +0530

    Pattern Program

---
+++ pattern5.py
@@ -0,0 +1,18 @@
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+
+rows = 5
+
+for i in range(1, rows + 1):
+    # spaces
+    for j in range(rows - i):
+        print(" ", end="")
+
+    # stars
+    for k in range(i):
+        print("* ", end="")
+
+    print()
